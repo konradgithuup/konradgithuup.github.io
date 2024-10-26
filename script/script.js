13710168895036ed068b9c2d943bc0be6b378dfa
Konrad Ueltzen <konrad.ueltzen@st.ovgu.de>
2024-10-26 18:11:09 +0200

    Add projects

--- script/script.js
+++ script/script.js
@@ -18,3 +18,55 @@ function loadGithubProfile(id) {
         link.href = user.avatar_url;
     });
 }
+
+function loadGithubProjects(id) {
+    var timespans;
+    timespans = fetch("resources/projects.json").then(re => re.json);
+    var parent = document.getElementById(id);
+
+
+    window.fetch(GITHUB_PROFILE + "/repos").then(function (response) {
+        return response.json();
+    }).then(function (repos) {
+
+        fetch("resources/projects.json").then(re => re.json()).then(function (timespans) {
+            timespans.forEach(timespan => {
+                createTimespanHeading(parent, timespan.timespan);
+                timespan.projects.forEach(project => {
+                    repos.forEach(repo => {
+                        if (project.id == repo.id) {
+                            createProjectPanel(parent, project, repo);
+                        }
+                    })
+                })
+            })
+        })
+    });
+}
+
+function createTimespanHeading(parent, timespan) {
+    var title = document.createElement("div");
+    title.setHTMLUnsafe("<h2>" + timespan + "</h2>");
+    parent.appendChild(title);
+}
+
+function createProjectPanel(parent, project, repo) {
+    var name = repo.name;
+    var link = repo.html_url;
+    var stars = repo.stargazers_count;
+
+    var container = document.createElement("div");
+    container.classList.add("repo");
+
+    var title = document.createElement("a");
+    title.classList.add("repoLink");
+    title.setAttribute("href", link);
+    title.textContent = name + " (★ " + stars + ")";
+
+    var body = document.createElement("div");
+    body.textContent = project.description;
+
+    container.appendChild(title);
+    container.appendChild(body);
+    parent.appendChild(container);
+}
